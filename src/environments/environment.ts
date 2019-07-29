d7cd7ac815a30491a032caf49fcc25ca60d6fffb
VladKachenya <38651074+VladKachenya@users.noreply.github.com>
2019-07-29 12:16:32 +0300

    added connection with back end

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 import 'zone.js/dist/zone-error';
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:56952/',
+  production: true,
+  apiUrl: 'https://katlasport.azurewebsites.net/',
+  //apiUrl: 'http://localhost:56952/',
 };
